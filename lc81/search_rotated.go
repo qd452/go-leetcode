7de152f9294aeeb74391ba1e77da0276ba165ff0
Qu Dong <qudong452@gmail.com>
2019-11-02 14:38:16 +0800

    search in rorated sorted array 2

---
+++ lc81/search_rotated.go
@@ -0,0 +1,29 @@
+package lc81
+
+func search(nums []int, target int) bool {
+	lo, hi := 0, len(nums)-1
+	for lo <= hi {
+		mid := (lo + hi) / 2
+		if nums[mid] == target {
+			return true
+		}
+		for lo < mid && nums[lo] == nums[mid] {
+			lo++
+		}
+		if nums[lo] <= nums[mid] {
+			if nums[lo] <= target && target < nums[mid] {
+				hi = mid - 1
+			} else {
+				lo = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[hi] {
+				lo = mid + 1
+			} else {
+				hi = mid - 1
+			}
+		}
+	}
+
+	return false
+}
