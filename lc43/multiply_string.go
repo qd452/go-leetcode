96886daaddb90de57e83f3a847f556ed2e15ff5c
Qu Dong <qudong452@gmail.com>
2019-11-02 21:52:24 +0800

    add lc43 math/big method [todo]: add normal method

---
+++ lc43/multiply_string.go
@@ -0,0 +1,17 @@
+package lc43
+
+import (
+	"math/big"
+)
+
+func multiply(num1 string, num2 string) string {
+    // i1, _ := strconv.Atoi(num1)
+    // i2, _ := strconv.Atoi(num2)
+    // big1 := big.NewInt(int64(i1))
+    // big2 := big.NewInt(int64(i2))
+    big1, _ := new(big.Int).SetString(num1, 0)
+    big2, _ := new(big.Int).SetString(num2, 0)
+    // fmt.Println(big1, big2)
+    big1.Mul(big1, big2)
+    return big1.String()
+}
