fbd7402b43b91e3f88aef03de0e375f578e27efb
Qu Dong <dong.qu@acronis.com>
2019-11-07 14:26:14 +0800

    add heap implementation in golang

--- go_example.go
+++ go_example.go
@@ -4,7 +4,30 @@ import (
 	"strings"
 	"fmt"
 	"sort"
+	"container/heap"
 )
+// An IntHeap is a min-heap of ints.
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+// Push - heappush
+func (h *IntHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+// Pop - heappop
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
 
 func main() {
 	s := "great"
@@ -19,4 +42,14 @@ func main() {
 	fmt.Println(sorted)
 	s2 = strings.Join(sorted, "")
 	fmt.Println(s2)
+
+	// This example inserts several ints into an IntHeap, checks the minimum,
+	// and removes them in order of priority.
+	h := &IntHeap{2, 1, 5}
+	heap.Init(h)
+	heap.Push(h, 3)
+	fmt.Printf("minimum: %d\n", (*h)[0])
+	for h.Len() > 0 {
+		fmt.Printf("%d ", heap.Pop(h))
+	}
 }
